d8aab9d7abe444ff0e101d82d2927410fe2c9dce
Eslam3bdel3al <abdelalleslam@gmail.com>
2022-07-03 12:07:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const {ApolloServer, gql} = require ("apollo-server");
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+  });
+  
+  // The `listen` method launches a web server.
+  server.listen(4000).then(() => {
+    console.log(`🚀  Server ready at http://localhost:4000/`);
+  });
